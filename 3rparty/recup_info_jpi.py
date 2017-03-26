b99260ce99390eefc8b104d37bc88da6d017007f
jmz84 <jmz84@users.noreply.github.com>
2017-03-26 13:57:12 +0200

    Delete recup_info_jpi.py

--- 3rparty/recup_info_jpi.py
+++
@@ -1,5 +0,0 @@
-import requests
-url_address = ['http://192.168.1.201:8080/?action=__NET_CMD__&__FROM_MAIN_APP__=true&net=action&action_ex=_GET_ACTIONS_EVENTS_']
-for i in url_address:
-    resp = requests.get(i).json()
-print resp
