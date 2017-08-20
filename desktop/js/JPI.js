5d8693b554e0736cdf0c78d7ee1197b100cffcf9
jmz84 <jerom@PC-BUREAU.kamehouse.local>
2017-08-20 16:24:50 +0200

    Fix ui

--- desktop/js/JPI.js
+++ desktop/js/JPI.js
@@ -124,7 +124,7 @@ function addCmdToTable(_cmd) {
 
         tr += '</td>';
 
-        tr += '<td class="expertModeVisible">';
+        tr += '<td>';
         tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>';
         tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>';
         tr += '</td>';
