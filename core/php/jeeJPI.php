3329bb919dac2da3d85b9f933d2350caa775f1b7
jmz84 <jerom@PC-BUREAU.kamehouse.local>
2017-08-20 12:12:41 +0200

    Log improvement

--- core/php/jeeJPI.php
+++ core/php/jeeJPI.php
@@ -17,16 +17,6 @@
  */
 require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
 
-//if (isset($argv)) {
-//    foreach ($argv as $arg) {
-//        $argList = explode('=', $arg);
-//        if (isset($argList[0]) && isset($argList[1])) {
-//            $_REQUEST[$argList[0]] = $argList[1];
-//        }
-//    }
-//}
-
-
 if (init('reponse') != '') {
     try {
         $type = init('reponse');
