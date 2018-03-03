19df1fae59f890723103a2db5a21f36e3248a751
Jerome MARTINEZ <jerome.martinez@live.fr>
2018-03-03 14:28:49 +0100

    Indent

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -1,19 +1,19 @@
 <?php
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
@@ -35,17 +35,17 @@ if (!isConnect()) {
                     <option value="30min">{{Toutes les 30 minutes}}</option>
                     <option value="60min">{{Toutes les heures}}</option>
                 </select>
-   </div>
+            </div>
 
 
-    </fieldset>
-</form>
+        </fieldset>
+    </form>
 
-<div class="row">
-  <div class="col-lg-6 text-center"><img src="plugins/JPI/resources/PAW.png" height="200" width="200" /></div>
-  <div class="col-lg-6 text-center"><img src="plugins/JPI/resources/APK.png" height="200" width="200" /></div>
-</div>
-<div class="row">
-  <div class="col-lg-6 text-center">Lien Paw</div>
-  <div class="col-lg-6 text-center">Lien JPI</div>
-</div>
+    <div class="row">
+        <div class="col-lg-6 text-center"><img src="plugins/JPI/resources/PAW.png" height="200" width="200" /></div>
+        <div class="col-lg-6 text-center"><img src="plugins/JPI/resources/APK.png" height="200" width="200" /></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 text-center">Lien Paw</div>
+        <div class="col-lg-6 text-center">Lien JPI</div>
+    </div>
