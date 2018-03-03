809fa15a1849f72a35df5bde59062f4a50ffc04f
Jerome MARTINEZ <jerome.martinez@live.fr>
2018-03-03 14:27:19 +0100

    Add dedicated API

--- core/php/jeeJPI.php
+++ core/php/jeeJPI.php
@@ -1,37 +1,36 @@
-<?php
-
-/* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
-require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
-if (!jeedom::apiAccess(init('apikey', init('api')))) {
-    echo __('Vous n\'etes pas autorisé à effectuer cette action (JPI)', __FILE__);
-    log::add('JPI', 'error', 'Vous n\'etes pas autorisé à effectuer cette action (JPI)');
+    <?php
+    /* This file is part of Jeedom.
+     *
+     * Jeedom is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * Jeedom is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+     */
+    require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
+    if (!jeedom::apiAccess(init('apikey'), 'JPI') && !jeedom::apiAccess(init('apikey'), 'API')) {
+        echo __('Vous n\'etes pas autorisé à effectuer cette action (JPI)', __FILE__);
+        log::add('JPI', 'error','Vous n\'etes pas autorisé à effectuer cette action (JPI)');
     die();
-}
-if (init('test') != '') {
-    echo 'OK';
-    die();
-}
-$reponse  = init('reponse');
-log::add('JPI', 'info', 'Réponse Ask : ' . $reponse);
-$eqLogics = eqLogic::byType('JPI');
-foreach ($eqLogics as $eqLogic) {
-    foreach ($eqLogic->getCmd('action') as $cmd) {
-        if ($cmd->askResponse($reponse)) {
-            die();
+    }
+    if (init('test') != '') {
+        echo 'OK';
+        die();
+    }
+    $reponse = init('reponse');
+    log::add('JPI', 'info', 'Réponse Ask : ' . $reponse);
+    $eqLogics = eqLogic::byType('JPI');
+    foreach ($eqLogics as $eqLogic) {
+        foreach ($eqLogic->getCmd('action') as $cmd) {
+            if ($cmd->askResponse($reponse)) {
+                die();
+            }
         }
     }
-}
