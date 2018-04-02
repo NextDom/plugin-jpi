aff31a75e77b329ab0c2064a8d464983bc83b42a
Jerome MARTINEZ <jerome.martinez@live.fr>
2018-04-02 09:37:20 +0200

    Fix parent

--- desktop/php/JPI.php
+++ desktop/php/JPI.php
@@ -41,12 +41,12 @@ $eqLogics = eqLogic::byType($plugin->getId());
         <div class="eqLogicThumbnailContainer">
             <?php
             foreach ($eqLogics as $eqLogic) {
-            $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-            echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-            echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
-            echo "<br>";
-            echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
-            echo '</div>';
+                $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+                echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+                echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
+                echo "<br>";
+                echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
+                echo '</div>';
             }
             ?>
         </div>
@@ -85,13 +85,15 @@ $eqLogics = eqLogic::byType($plugin->getId());
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-4 control-label" >{{Objet parent}}</label>
-                                    <div class="col-sm-6">
+                                    <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                                    <div class="col-sm-3">
                                         <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
                                             <option value="">{{Aucun}}</option>
-                                            <?php foreach (object::all() as $object) : ?>
-                                                <option value="' . $object->getId() . '"><?= $object->getName() ?></option>';
-                                            <?php endforeach ?>
+                                            <?php
+                                            foreach (object::all() as $object) {
+                                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                                            }
+                                            ?>
                                         </select>
                                     </div>
                                 </div>
@@ -142,71 +144,70 @@ $eqLogics = eqLogic::byType($plugin->getId());
                                     </div>
                                 </div>
 
-                                </div>
-                            </fieldset>
-                        </form>
+                            </div>
+                        </fieldset>
+                    </form>
 
-                        <?php
-                        if (network::getUserLocation() == 'internal') {
-                            echo '<div class="col-sm-5">';
-                            echo '  <form class="form-horizontal">';
-                            echo '     <fieldset>';
-                            echo '       <div class="cursor" id="bt_Device">';
-                            echo '          <center>';
-                            echo '            <i class="fa fa-mobile" style="font-size : 5em;color:#767676;"></i>';
-                            echo '       </center>';
-                            echo '      <span style="font-size : 1.1em;position:relative;word-break: break-all;white-space: pre-wrap;word-wrap: break-word"><center>{{Lien vers équipement JPI}}</center></span>';
-                            echo '  </div>';
-                            echo ' </form>';
-                            echo ' </div> ';
-                        }
-                        ?>
-                    </div>
-
+                    <?php
+                    if (network::getUserLocation() == 'internal') {
+                        echo '<div class="col-sm-5">';
+                        echo '  <form class="form-horizontal">';
+                        echo '     <fieldset>';
+                        echo '       <div class="cursor" id="bt_Device">';
+                        echo '          <center>';
+                        echo '            <i class="fa fa-mobile" style="font-size : 5em;color:#767676;"></i>';
+                        echo '       </center>';
+                        echo '      <span style="font-size : 1.1em;position:relative;word-break: break-all;white-space: pre-wrap;word-wrap: break-word"><center>{{Lien vers équipement JPI}}</center></span>';
+                        echo '  </div>';
+                        echo ' </form>';
+                        echo ' </div> ';
+                    }
+                    ?>
                 </div>
 
-                <div role="tabpanel" class="tab-pane" id="commandtabCmdWidget">
-                    <table id="table_cmdWidget" class="table table-bordered table-condensed">
-                        <thead>
-                            <tr>
-                                <th style="width: 50px;"></th>
-                                <th style="width: 150px;">{{Nom}}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
-                </div>
-
-                <div role="tabpanel" class="tab-pane" id="commandtab">
-                    <a class="btn btn-success pull-right" id="bt_Assistant" ><i class="fa fa-plus-circle"></i> {{Assistant de commande JPI}}</a>
-                    <a class="btn btn-success cmdAction pull-right" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande JPI}}</a><br/><br/>
-                    <table id="table_cmd" class="table table-bordered table-condensed">
-                        <thead>
-                            <tr>
-                                <th style="width: 65px;"></th>
-                                <th style="width: 10px;"></th>
-                                <th style="width: 100px;">{{Nom}}</th>
-                                <th style="width: 100px;">{{Actions}}</th>
-                                <th style="width: 280px;">{{Paramètres}}</th>
-                                <th style="width: 280px;">{{Options}}</th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
-                </div>
             </div>
 
+            <div role="tabpanel" class="tab-pane" id="commandtabCmdWidget">
+                <table id="table_cmdWidget" class="table table-bordered table-condensed">
+                    <thead>
+                        <tr>
+                            <th style="width: 50px;"></th>
+                            <th style="width: 150px;">{{Nom}}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+
+            <div role="tabpanel" class="tab-pane" id="commandtab">
+                <a class="btn btn-success pull-right" id="bt_Assistant" ><i class="fa fa-plus-circle"></i> {{Assistant de commande JPI}}</a>
+                <a class="btn btn-success cmdAction pull-right" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une commande JPI}}</a><br/><br/>
+                <table id="table_cmd" class="table table-bordered table-condensed">
+                    <thead>
+                        <tr>
+                            <th style="width: 65px;"></th>
+                            <th style="width: 10px;"></th>
+                            <th style="width: 100px;">{{Nom}}</th>
+                            <th style="width: 100px;">{{Actions}}</th>
+                            <th style="width: 280px;">{{Paramètres}}</th>
+                            <th style="width: 280px;">{{Options}}</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
+
     </div>
+</div>
 </div>
 
 <?php
 include_file('desktop', 'JPI', 'js', 'JPI');
 
 include_file('core', 'plugin.template', 'js');
-
